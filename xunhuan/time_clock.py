55ed5fe0d55573e133bd494ce65d6b0e7869d706
unwen <494895913@qq.com>
2016-10-06 10:15:23 -0400

    add time

---
+++ xunhuan/time_clock.py
@@ -0,0 +1,7 @@
+import time 
+
+begin_time=time.clock()
+while "a":
+
+    over_time=time.clock()
+    print(over_time)
--- xunhuan/while.py
+++ xunhuan/while.py
@@ -1,4 +1,11 @@
+import time
+
 i=0
+begin_time=time.clock()
+
+
 while "c":
     i+=1
-    print ("I love   "+str(i))
+    over_time=time.clock()
+    
+    print ("I love   "+str(i)+"  "+str(i/over_time))
